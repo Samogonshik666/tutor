1584fd8de37e3358a733ff333c1163cdfd3f8421
Samogonshik666 <samogonshik6@gmail.com>
2022-08-18 21:05:00 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+import './src/style.css'
+import './src/normalize.css'
+import './src/header.css'
+import './src/info.css'
+import './src/directions.css'
+import './src/lessons.css' 
+import './src/videolesson.css' 
+import './src/reviews.css' 
+import './src/footer.css'
+import './src/mobile.css'
+
+
+import '@splidejs/splide/css/core';
+import Splide from '@splidejs/splide';
+new Splide( '.splide' ).mount();
+
+
+
+
+
+
